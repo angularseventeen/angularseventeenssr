f2c18aba038c81c24c5ccdd7608f88bc022a383d
Kushal Hada <kus@tilde.team>
2024-03-09 07:17:01 -0500

    end prepare to build angular

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FCR4GWT2.mjs";import"./chunk-2BH2YOIP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-4UTZODQI.mjs";import"./chunk-XZZGI7US.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
